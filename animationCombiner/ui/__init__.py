6f9f814ba1862c352c8847b5e6963951f0aac89b
pehala <halapetr@selfnet.cz>
2023-01-21 18:24:38 +0100

    Change Play button to Pause when playing

--- animationCombiner/ui/__init__.py
+++ animationCombiner/ui/__init__.py
@@ -36,5 +36,8 @@ class ControlPanel(bpy.types.Panel):
         col = layout.column(align=True)
         row = col.row(align=True)
         col.prop(bpy.context.scene.render, "fps", text="FPS", slider=True)
-        row.operator(RunAnimationOperator.bl_idname, text="Play", icon="PLAY")
+        if bpy.context.screen.is_animation_playing:
+            row.operator(RunAnimationOperator.bl_idname, text="Stop", icon="PAUSE")
+        else:
+            row.operator(RunAnimationOperator.bl_idname, text="Play", icon="PLAY")
         row.operator(BackToStartOperator.bl_idname, text="Back To Start", icon="PREV_KEYFRAME")
