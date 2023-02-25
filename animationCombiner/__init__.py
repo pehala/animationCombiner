211b8fe2cc4a76ff5234c1f8d0537149c6a41ae7
Petr Hála <halapetr@selfnet.cz>
2023-02-25 15:20:25 +0100

    Bump version to 0.0.3

--- animationCombiner/__init__.py
+++ animationCombiner/__init__.py
@@ -7,7 +7,7 @@ bl_info = {
     "name": "Animation Combiner",
     "author": "phala",
     "description": "Blender plugin for crafting new animations from existing ones",
-    "version": (0, 0, 2, 1),
+    "version": (0, 0, 3),
     "blender": (3, 0, 0),
     "location": "View3D -> AnimationCombiner",
     "warning": "",
