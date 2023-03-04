9f45b73cb45c89a9552593beaf915c2c4267f5d5
pehala <halapetr@selfnet.cz>
2023-03-04 22:36:18 +0100

    Fix Apply with Groups

--- animationCombiner/animation.py
+++ animationCombiner/animation.py
@@ -49,11 +49,16 @@ def create_armature(name: str = "Armature"):
     return armature
 
 
-def process_animation(armature, action: Action, base_skeleton, frame_start=0, frame_delay=1):
+def process_animation(armature, action: Action, base_skeleton, parts, frame_start=0, frame_delay=1):
     """Test"""
     animation = action.animation
     order = animation.order
+    disabled_parts = {part.uuid for part in action.body_parts if not part.checked}
 
+    disabled_bones = {"root"}
+    for uuid, bones in parts.items():
+        if uuid in disabled_parts:
+            disabled_bones.update(bones)
     # # Set it to base rotation
     # for name, rotation in zip(order, base_skeleton):
     #     bone = armature.pose.bones[name]
@@ -69,21 +74,23 @@ def process_animation(armature, action: Action, base_skeleton, frame_start=0, fr
     for i, frame in enumerate(animation.animation[action.length_group.start : action.length_group.end]):
         last_frame = frame_start + (i * frame_delay)
         for name, rotation in zip(order, frame.rotations):
-            bone = armature.pose.bones[name]
-            bone.rotation_quaternion = rotation.rotation
-            bone.keyframe_insert(
-                data_path="rotation_quaternion",
-                frame=last_frame,
-                group=name,
-            )
+            if name not in disabled_bones:
+                bone = armature.pose.bones[name]
+                bone.rotation_quaternion = rotation.rotation
+                bone.keyframe_insert(
+                    data_path="rotation_quaternion",
+                    frame=last_frame,
+                    group=name,
+                )
     if action.transition.reset:
         last_frame = last_frame + action.transition.reset_length
         for name, rotation in zip(order, base_skeleton):
-            bone = armature.pose.bones[name]
-            bone.rotation_quaternion = rotation
-            bone.keyframe_insert(
-                data_path="rotation_quaternion",
-                frame=last_frame,
-                group=name,
-            )
+            if name not in disabled_bones:
+                bone = armature.pose.bones[name]
+                bone.rotation_quaternion = rotation
+                bone.keyframe_insert(
+                    data_path="rotation_quaternion",
+                    frame=last_frame,
+                    group=name,
+                )
     return last_frame + action.transition.length
